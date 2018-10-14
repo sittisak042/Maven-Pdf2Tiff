7b8b8a480c890981a249e2b95b5f187bb12ee99c
sittisak <sittisak.k@appman.co.th>
2018-10-14 17:42:13 +0700

    first commit

---
+++ src/main/java/com/appman/maven/pdf2tiff/Pdf2Tiff.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.appman.maven.pdf2tiff;
+
+import org.ghost4j.Ghostscript;
+import org.ghost4j.GhostscriptException;
+
+/**
+ *
+ * @author APPMAN_LABTOP_56
+ */
+public class Pdf2Tiff {
+    
+    private static String iPath = "FixFile/";
+    private static String oPath = "Result/";
+    
+    public static void main(String[] args){
+       
+       String pdfName = "";
+       String tiffName = "";
+       String filenames[] = {
+           "3033255500"
+       };
+       
+       for (int i = 0; i < filenames.length; i++){
+           pdfName = iPath + "application-" + filenames[i] + ".pdf";
+           tiffName = oPath + "application-" + filenames[i ]+ ".tiff";
+
+            Ghostscript gs = Ghostscript.getInstance();
+            String[] gsArgs = new String[] {
+                    "-dQUIET", "-dNOPAUSE", "-dBATCH", "-dSAFER",
+                    "-sDEVICE=tiffgray", "-sCompression=lzw", "-r200", "-sOutputFile=" + tiffName, pdfName
+            };
+            try {
+                gs.initialize(gsArgs);
+                System.out.print(filenames[i]);
+                gs.exit();
+                System.out.println(" Done");
+            } catch (GhostscriptException e) {
+                System.out.println(e.getMessage());
+            }
+       }
+    }
+}
